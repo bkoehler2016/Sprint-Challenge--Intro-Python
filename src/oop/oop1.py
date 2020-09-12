977d1bf8d9f6b71de9c9f843387ccaf1b0260bb3
Benjamin Koehler <bkoehler751@gmail.com>
2020-09-11 18:14:10 -0700

    completed oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -21,3 +21,20 @@
 class Vehicle: ## BASE CLASS ##
     pass
 
+class FlightVehicle(Vehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
